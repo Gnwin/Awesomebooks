45cf4201e073e75d92280df15b180d6fd162a875
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-24 15:23:53 +0100

    make page functional

--- awesome.js
+++ awesome.js
@@ -1,51 +1,57 @@
+let addBtn = document.querySelector('#addBtn');
+let input = document.querySelectorAll('[type="text"]');
+let remBtns = document.querySelectorAll('.removeBtn');
+
 let books = [
-  {
-    id: 0,
-    title: 'strong men',
-    author: 'godwin'
-  },
-  {
-    id: 1,
-    title: 'javascript',
-    author: 'chioma'
-  }
+  // {
+  //   id: 0,
+  //   title: 'strong men',
+  //   author: 'godwin'
+  // },
+  // {
+  //   id: 1,
+  //   title: 'javascript',
+  //   author: 'chioma'
+  // }
 ];
 
 function addBook(title, author){
   var id = Math.random().toString(16).slice(2);
   books.push({id, title, author});
-  console.log(booklist)
+  console.log(booklist);
 }
 
-function removeBook(title, author){
+function removeBook(id){
   books = books.filter(book => {
-    return book.title !== title || book.author !== author;
+    return book.id !== id;
   })
 }
 
 function displayBook(){
   let bookList = document.getElementById('booklist');
-
   let markup = '';
-  books.forEach(book => {
-    let element = `<div class='book'>
-      <div class=''>
-        <p>${book.title}</p>
-        <p><small>by ${book.author}</small></p>
-      </div>
-      <button class='removeBtn'>Remove</button>
-    </div>`;
-    markup += element;
-  })
-  console.log(markup);
+  let element;
+  if (books.length === 0) {
+    element = `<div>No Books to display. Click add new to add a book to the list<div>`;
+    markup = element;
+  } else {
+    books.forEach(book => {
+      element = `<div class='book'>
+        <div class=''>
+          <p>${book.title}</p>
+          <small>by</small>
+          <p><small>${book.author}</small></p>
+        </div>
+        <button class='removeBtn' onclick='remove(event);'>Remove</button>
+      </div>`;
+      markup += element;
+    })
+  }
   bookList.innerHTML = markup;
 }
 
 displayBook();
 
-let addBtn = document.querySelector('#addBtn');
-let input = document.querySelectorAll('[type="text"]');
-let remBtn = document.querySelector('.removeBtn');
 
 addBtn.addEventListener('click', (e)=>{
   e.preventDefault();
@@ -55,13 +61,25 @@ addBtn.addEventListener('click', (e)=>{
   let title = input[0].value;
   let author = input[1].value;
   console.log(title, author);
+
   addBook(title, author);
   input[0].value = '';
   input[1].value = '';
   displayBook();
-  return false;
+  remBtns = document.querySelectorAll('.removeBtn');
+  // return false;
 })
 
-// var id = Math.random().toString(16).slice(2);
-// console.log(id);
+
+let remove = (val) => {
+  let book = val.target.parentElement.children[0];
+  let title = book.children[0].innerHTML;
+  let author = book.children[2].children[0].innerHTML;
+  for (let i=0; i<books.length; i++){
+    if (books[i].title === title && books[i].author === author){
+      removeBook(books[i].id);
+    }
+  }
+  displayBook();
+}
 
