1c5f584164f806f58073a9baceb83c5c802fd871
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-24 20:22:54 +0100

    make page functional

--- awesome.js
+++ awesome.js
@@ -1,48 +1,41 @@
-let addBtn = document.querySelector('#addBtn');
 let input = document.querySelectorAll('[type="text"]');
-let remBtns = document.querySelectorAll('.removeBtn');
+let books = [];
 
-let books = [
-  // {
-  //   id: 0,
-  //   title: 'strong men',
-  //   author: 'godwin'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'javascript',
-  //   author: 'chioma'
-  // }
-];
+function setLocalStorage(){
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+displayBook();
 
 function addBook(title, author){
   var id = Math.random().toString(16).slice(2);
   books.push({id, title, author});
-  console.log(booklist);
+  setLocalStorage();
 }
 
 function removeBook(id){
-  books = books.filter(book => {
-    return book.id !== id;
-  })
+  books = books.filter(book => book.id !== id);
+  setLocalStorage()
 }
 
 function displayBook(){
+  if (localStorage.length === 0){
+    setLocalStorage();
+  }
+  const bookStorage = JSON.parse(localStorage.getItem('books'));
   let bookList = document.getElementById('booklist');
   let markup = '';
-  let element;
-  if (books.length === 0) {
-    element = `<div>No Books to display. Click add new to add a book to the list<div>`;
-    markup = element;
+  if (bookStorage.length === 0) {
+    markup = `<div class='pad'>No Books to display. Click add new to add a book to the list<div>`;
   } else {
-    books.forEach(book => {
-      element = `<div class='book'>
+    bookStorage.forEach(book => {
+      let element = `<div class='book'>
         <div class=''>
           <p>${book.title}</p>
           <small>by</small>
           <p><small>${book.author}</small></p>
         </div>
-        <button class='removeBtn' onclick='remove(event);'>Remove</button>
+        <button class='removeBtn' onclick = remove(event);>Remove</button>
       </div>`;
       markup += element;
     })
@@ -50,34 +43,30 @@ function displayBook(){
   bookList.innerHTML = markup;
 }
 
-displayBook();
-
-
-addBtn.addEventListener('click', (e)=>{
-  e.preventDefault();
+function add(event){
+  event.preventDefault();
   if(input[0].value === '' || input[1].value === ''){
     return;
   }
   let title = input[0].value;
   let author = input[1].value;
-  console.log(title, author);
-
   addBook(title, author);
   input[0].value = '';
   input[1].value = '';
   displayBook();
-  remBtns = document.querySelectorAll('.removeBtn');
   // return false;
-})
+}
 
 
-let remove = (val) => {
-  let book = val.target.parentElement.children[0];
+let remove = (event) => {
+  event.preventDefault();
+  const bookStorage = JSON.parse(localStorage.getItem('books'));
+  let book = event.target.parentElement.children[0];
   let title = book.children[0].innerHTML;
   let author = book.children[2].children[0].innerHTML;
-  for (let i=0; i<books.length; i++){
-    if (books[i].title === title && books[i].author === author){
-      removeBook(books[i].id);
+  for (let i=0; i<bookStorage.length; i++){
+    if (bookStorage[i].title === title && bookStorage[i].author === author){
+      removeBook(bookStorage[i].id);
     }
   }
   displayBook();
